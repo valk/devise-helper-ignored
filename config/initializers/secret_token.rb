b51a41662323d4e3b7d3853b670fca06a9854a4a
Val Kotlarov Hoffman <val.hoffman@gmail.com>
2014-06-27 08:52:25 +0300

    New project with devise gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ValTestDevise::Application.config.secret_key_base = 'cf483f1450ee605130530b7087ab472e98fb4f0afeb72f2ab3c8a070ea9c10006687e17c873d668d61a28a8970983cdcceef68aa8c8875ec6eafabfefb9b536d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ValTestDevise::Application.config.session_store :cookie_store, key: '_val-test-devise_session'
