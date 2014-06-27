a7aba86a1e6d2b80ff3c460a70770317721d8830
Val Kotlarov Hoffman <val.hoffman@gmail.com>
2014-06-27 08:55:51 +0300

    Satisfy Devise installation requirements

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ValTestDevise::Application.routes.draw do
+  root to: "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
