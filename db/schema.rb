07444665faa53d61c35d2c0a339183e89adf95ae
Val Kotlarov Hoffman <val.hoffman@gmail.com>
2014-06-27 09:01:05 +0300

    Create User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140627055729) do
+ActiveRecord::Schema.define(version: 20140627060038) do
 
   create_table "homes", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
